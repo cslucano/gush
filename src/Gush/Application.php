a9027242e7fc700d968401704531b13197b28b1e
dantleech <dan.t.leech@gmail.com>
2014-02-02 09:12:03 +0000

    Added command to add and update headers

--- src/Gush/Application.php
+++ src/Gush/Application.php
@@ -51,7 +51,7 @@ class Application extends BaseApplication
         $helperSet->set(new Helpers\TextHelper());
         $helperSet->set(new Helpers\TableHelper());
         $helperSet->set(new Helpers\ProcessHelper());
-        $helperSet->set(new Helpers\GitHelper($helperset->get('process')));
+        $helperSet->set(new Helpers\GitHelper($helperSet->get('process')));
         $helperSet->set(new Helpers\TemplateHelper($helperSet->get('dialog')));
 
         // the parent dispatcher is private and has
