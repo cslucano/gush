f8b71816e95486dd02789cbd9f78973ba2fdbfb4
Luis Cordova <cordoval@gmail.com>
2014-02-07 22:11:36 -0500

    plug image generator

--- src/Gush/Application.php
+++ src/Gush/Application.php
@@ -102,6 +102,7 @@ class Application extends BaseApplication
         $this->add(new Cmd\LabelIssuesCommand());
         $this->add(new Cmd\ConfigureCommand());
         $this->add(new Cmd\VersionEyeCommand());
+        $this->add(new Cmd\PullRequestImageGeneratorCommand());
     }
 
     /**
